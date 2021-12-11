f6b91fb540b562de31484cc93a9c5cf93e6227d7
Leonie Popken <leonie.popken@tum.de>
2021-12-11 22:09:53 +0100

    Added post request functionality

--- lib/screens/survey_screen.dart
+++ lib/screens/survey_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:survey_kit/survey_kit.dart';
 
 /// This screen contains the survey the user takes at the beginning and end of the campaign/lesson
@@ -28,7 +29,9 @@ class _SurveyScreenState extends State<SurveyScreen> {
                   final task = snapshot.data!;
                   return SurveyKit(
                     onResult: (SurveyResult result) {
-                      print(result.finishReason);
+                      if (result.finishReason == FinishReason.COMPLETED) {
+                        postResult(result);
+                      }
                     },
                     task: task,
                     themeData: Theme.of(context).copyWith(
@@ -121,17 +124,21 @@ class _SurveyScreenState extends State<SurveyScreen> {
       id: TaskIdentifier(),
       steps: [
         InstructionStep(
+          stepIdentifier: StepIdentifier(id: 'hello'),
           title: 'Welcome to \nBottys survey',
-          text: 'Get ready for a bunch of super interesting questions, so we can get to know each other!',
+          text:
+              'Get ready for a bunch of super interesting questions, so we can get to know each other!',
           buttonText: 'Let\'s go!',
         ),
 
         //TODO: Add survey questions here
         QuestionStep(
-            title: 'How old are you?',
-            answerFormat: IntegerAnswerFormat(),
+          stepIdentifier: StepIdentifier(id: 'age'),
+          title: 'How old are you?',
+          answerFormat: IntegerAnswerFormat(),
         ),
         QuestionStep(
+          stepIdentifier: StepIdentifier(id: 'gender'),
           title: 'How do you identify yourself?',
           answerFormat: const SingleChoiceAnswerFormat(
             textChoices: [
@@ -142,48 +149,52 @@ class _SurveyScreenState extends State<SurveyScreen> {
           ),
         ),
         QuestionStep(
-            title: 'Where do you see your data protection knowledge?',
-            answerFormat: const ScaleAnswerFormat(
+          stepIdentifier: StepIdentifier(id: 'knowledge'),
+          title: 'Where do you see your data protection knowledge?',
+          answerFormat: const ScaleAnswerFormat(
               step: 1,
               minimumValue: 1,
               maximumValue: 10,
               defaultValue: 5,
               minimumValueDescription: '1',
-              maximumValueDescription: '10'
-            ),
+              maximumValueDescription: '10'),
         ),
         QuestionStep(
+          stepIdentifier: StepIdentifier(id: 'contacts'),
           title: 'Wie oft bist du mit dem Thema schon in Kontakt gekommen',
-            answerFormat: const SingleChoiceAnswerFormat(
-              textChoices: [
-                TextChoice(text: 'nie', value: 'nie'),
-                TextChoice(text: 'selten', value: 'selten'),
-                TextChoice(text: 'manchmal', value: 'manchmal'),
-                TextChoice(text: 'oft', value: 'oft'),
-                TextChoice(text: 'ständig', value: 'ständig'),
-              ],
-            ),
+          answerFormat: const SingleChoiceAnswerFormat(
+            textChoices: [
+              TextChoice(text: 'nie', value: 'nie'),
+              TextChoice(text: 'selten', value: 'selten'),
+              TextChoice(text: 'manchmal', value: 'manchmal'),
+              TextChoice(text: 'oft', value: 'oft'),
+              TextChoice(text: 'ständig', value: 'ständig'),
+            ],
+          ),
         ),
         QuestionStep(
-            title: 'Hast du je ein (Lern-)Spiel mit einem Chatbot gespielt?',
-            answerFormat: const SingleChoiceAnswerFormat(
-              textChoices: [
-                TextChoice(text: 'ja', value: 'ja'),
-                TextChoice(text: 'nö', value: 'nö'),
-              ],
-            ),
+          stepIdentifier: StepIdentifier(id: 'playedBefore'),
+          title: 'Hast du je ein (Lern-)Spiel mit einem Chatbot gespielt?',
+          answerFormat: const SingleChoiceAnswerFormat(
+            textChoices: [
+              TextChoice(text: 'ja', value: 'ja'),
+              TextChoice(text: 'nö', value: 'nö'),
+            ],
+          ),
         ),
         QuestionStep(
-            title: 'Please tell us a bit about it ;D',
-            answerFormat: const TextAnswerFormat(
-              maxLines: 5,
-              validationRegEx: "^(?!\s*\$).+",
-            ),
+          stepIdentifier: StepIdentifier(id: 'tellus'),
+          title: 'Please tell us a bit about it ;D',
+          answerFormat: const TextAnswerFormat(
+            maxLines: 5,
+            validationRegEx: "^(?!\s*\$).+",
+          ),
         ),
 
         CompletionStep(
-          stepIdentifier: StepIdentifier(id: '321'),
-          text: 'Thanks for taking the survey, sounds like you are a cool person!',
+          stepIdentifier: StepIdentifier(id: 'completion'),
+          text:
+              'Thanks for taking the survey, sounds like you are a cool person!',
           title: 'Done!',
           buttonText: 'Submit survey',
         ),
@@ -192,24 +203,38 @@ class _SurveyScreenState extends State<SurveyScreen> {
 
     //TODO: Add navigation rule
     task.addNavigationRule(
-        forTriggerStepIdentifier: task.steps[5].stepIdentifier,
-        navigationRule: ConditionalNavigationRule(
-            resultToStepIdentifierMapper: (input) {
-              switch(input){
-                case 'ja':
-                  return task.steps[6].stepIdentifier;
-                case 'nö':
-                  return task.steps[7].stepIdentifier;
-                default:
-                  return null;
-              }
-            },
-        ),
+      forTriggerStepIdentifier: task.steps[5].stepIdentifier,
+      navigationRule: ConditionalNavigationRule(
+        resultToStepIdentifierMapper: (input) {
+          switch (input) {
+            case 'ja':
+              return task.steps[6].stepIdentifier;
+            case 'nö':
+              return task.steps[7].stepIdentifier;
+            default:
+              return null;
+          }
+        },
+      ),
     );
 
     return Future.value(task);
   }
 
+  Future<http.Response> postResult(SurveyResult result) {
+    var map = <String, dynamic>{};
+    for (var res in result.results) {
+      if (res.id != null &&
+          res.id!.id != "hello" &&
+          res.id!.id != "completion") {
+        map[res.id!.id] = res.results[0].valueIdentifier;
+      }
+    }
+    return http.post(
+        Uri.parse(
+            'https://botty-datenschutz.de/wp-json/contact-form-7/v1/contact-forms/155/feedback'),
+        body: map);
+  }
 
 //This one may be needed in the future
 /*Future<Task> getJsonTask() async {
